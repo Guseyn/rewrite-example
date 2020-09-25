135bce822a0e8f92fc6fe1078196be27db7b131c
Sam Snyder <sam@moderne.io>
2020-09-25 02:09:11 -0700

    Rewrite ready to run

---
+++ src/test/java/org/openrewrite/Sample.java
@@ -0,0 +1,11 @@
+package org.openrewrite;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+class Sample {
+    @Test
+    void sample() {
+        Assertions.assertEquals(42, 21*2);
+    }
+}
